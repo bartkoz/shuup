da17464364947751e1ed59f3d9383636d44fc28b
bartkoz <bartmkoz@gmail.com>
2021-12-04 21:20:45 +0400

    neeeded changes

--- shuup/front/checkout/payment_type.py
+++ shuup/front/checkout/payment_type.py
@@ -1,37 +1,42 @@
 from django import forms
+from django.utils.safestring import mark_safe
 from django.views.generic import FormView
 from django.utils.translation import ugettext_lazy as _
 
 from shuup.front.checkout import CheckoutPhaseViewMixin
 from shuup.utils.form_group import FormGroup
 from shuup_stripe_multivendor.context_provider import get_payment_context_provider
-from shuup.core.models import Order, get_person_contact
+from shuup.core.models import get_person_contact
 
 
 class PaymentTypeForm(forms.Form):
 
-    payment_method = forms.ChoiceField(choices=[('p24', 'Przelewy 24'), ('card', 'Karta')],
-                                       widget=forms.RadioSelect, label=_('Metoda Płatności'))
+    payment_method = forms.ChoiceField(choices=[('p24', mark_safe('Przelewy 24 <img src="https://movyu-prod.s3.eu-west-1.amazonaws.com/przelewy24_logo.svg" alt="przelewy24" style="width: 40px; height: 26px;"')), ('card',
+                                                                                    mark_safe('Karta  <img src="https://movyu-prod.s3.eu-west-1.amazonaws.com/visa.svg" style="width: 40px; height: 26px;" alt="visa"><img src="https://movyu-prod.s3.eu-west-1.amazonaws.com/mastercard.svg" style="width: 40px; height: 26px;"alt="mastercard">'))],
+                                       widget=forms.RadioSelect(attrs={'class': "custom-radio-list"}), label=_('Metoda Płatności'))
 
 
 class PaymentTypeFormCardLocked(forms.Form):
 
-    payment_method = forms.ChoiceField(choices=[('p24', 'Przelewy 24'), ('card', 'Karta')],
-                                       widget=forms.RadioSelect, label=_('Metoda Płatności'),
+    payment_method = forms.ChoiceField(choices=[('p24', mark_safe('Przelewy 24 <img src="https://movyu-prod.s3.eu-west-1.amazonaws.com/przelewy24_logo.svg" alt="przelewy24" style="width: 40px; height: 26px;"')), ('card',
+                                                                                    mark_safe('Karta  <img src="https://movyu-prod.s3.eu-west-1.amazonaws.com/visa.svg" style="width: 40px; height: 26px;" alt="visa"><img src="https://movyu-prod.s3.eu-west-1.amazonaws.com/mastercard.svg" style="width: 40px; height: 26px;"alt="mastercard">'))],
+                                       widget=forms.RadioSelect(attrs={'class': "custom-radio-list"}), label=_('Metoda Płatności'),
                                        disabled=True,
                                        initial='card')
 
+
 class PaymentTypeFormP224Locked(forms.Form):
 
-    payment_method = forms.ChoiceField(choices=[('p24', 'Przelewy 24'), ('card', 'Karta')],
-                                       widget=forms.RadioSelect, label=_('Metoda Płatności'),
+    payment_method = forms.ChoiceField(choices=[('p24', mark_safe('Przelewy 24 <img src="https://movyu-prod.s3.eu-west-1.amazonaws.com/przelewy24_logo.svg" alt="przelewy24" style="width: 40px; height: 26px;"')), ('card',
+                                                                                    mark_safe('Karta  <img src="https://movyu-prod.s3.eu-west-1.amazonaws.com/visa.svg" style="width: 40px; height: 26px;" alt="visa"><img src="https://movyu-prod.s3.eu-west-1.amazonaws.com/mastercard.svg" style="width: 40px; height: 26px;"alt="mastercard">'))],
+                                       widget=forms.RadioSelect(attrs={'class': "custom-radio-list"}), label=_('Metoda Płatności'),
                                        disabled=True,
                                        initial='p24')
 
 
 class PaymentType(CheckoutPhaseViewMixin, FormView):
     identifier = "payment_type"
-    title = _("Wybierz metodę wysyłki")
+    title = _("Wybierz metodę płatności")
 
     template_name = "shuup/front/checkout/payment_method.jinja"
 
